f4cf1e73f407dfccf84d15c61fd129b5619cd90c
Nicholas Matias <113799954+NicholasMatias@users.noreply.github.com>
2024-06-14 03:25:34 -0700

    App Title update: Movies -> Flixster

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return(
   <div className="App">
     <header className='App-header'>
-      <h1 className='site-title'>Movies</h1>
+      <h1 className='site-title'>Flixster</h1>
     </header>
     <main>
       <MovieList />
